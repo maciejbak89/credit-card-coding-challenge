38c688b4b1c3d2009bb174e5c4173f902f33c25e
Maciej Bak <mbak704@gmail.com>
2023-10-06 17:29:00 +0200

    refactored TweenNumber animation from CreditCards.vue

---
+++ src/animations/TweenNumber.vue
@@ -0,0 +1,83 @@
+<template>
+  <span>{{ formattedNum }}</span>
+</template>
+
+<script>
+import gsap from "gsap";
+import { mapGetters } from "vuex";
+
+export default {
+  // The code below was written to provide a tween effect on the entered credit card number.
+  // This effect was created using the GSAP animation library (https://greensock.com/docs/v3/GSAP/gsap.to()).
+  props: {
+    start: Number,
+    end: Number,
+    displayInit: Number,
+  },
+
+  data: () => ({
+    number: { num: 0, tweenedNum: 0 },
+    substringStart: 0,
+    substringEnd: 0,
+  }),
+
+  created() {
+    // Initiate the number object that will be used to create the tween animation with values passed
+    // as props from the parent. Use a default initiation display value from the parent and define
+    // the substring start and end points for the watcher on the changing form credit card number:
+    this.number = {
+      num: this.displayInit,
+      tweenedNum: this.displayInit,
+    };
+    this.substringStart = this.start;
+    this.substringEnd = this.end;
+  },
+
+  methods: {
+    // Method that defines the tween using the to() method that defines the destination value of
+    // the tween and the duration of the animation:
+    tweenNum(newNumber) {
+      gsap.to(this.number, { duration: 0.2, tweenedNum: newNumber });
+    },
+  },
+
+  computed: {
+    ...mapGetters(["getFormData", "getCreditCardType", "getCreditCardFormat"]),
+    formattedNum() {
+      return Math.round(this.number.tweenedNum)
+        ? Math.round(this.number.tweenedNum)
+        : this.getFormData.cardNumber.substring(
+            this.substringStart,
+            this.substringEnd
+          )
+        ? 0
+        : "";
+      // Double ternary operator is used for credit card numbers that are equal to 0 so
+      // that 0's are also displayed, otherwise display an empty space is the value is empty.
+    },
+  },
+
+  watch: {
+    // Watch the changing credit card number from the Vuex state of formData to update the
+    // number object accordingly:
+    "$store.getters.getFormData"() {
+      this.number.num = Number(
+        this.getFormData.cardNumber.substring(
+          this.substringStart,
+          this.substringEnd
+        )
+      );
+    },
+
+    // Using the above watcher for the Vuex formData cardNumber, watch for changes on each credit
+    // card number and run the tweenNum method on each value change to achieve an individual tween
+    // animation for each credit card number:
+    number: {
+      handler(newValue) {
+        this.tweenNum(newValue.num);
+      },
+      deep: true,
+    },
+  },
+};
+</script>
--- src/components/payments/CreditCards.vue
+++ src/components/payments/CreditCards.vue
@@ -25,41 +25,14 @@
           width="32"
         />
         <div class="credit-card-text credit-card-numbers font-weight-medium">
-          <div v-if="getCreditCardFormat === 1">
-            <span>{{ formattedOne }}</span>
-            <span>{{ formattedTwo }}</span>
-            <span>{{ formattedThree }}</span>
-            <span class="mr-1">{{ formattedFour }}</span>
-            <span>{{ formattedFive }}</span>
-            <span>{{ formattedSix }}</span>
-            <span>{{ formattedSeven }}</span>
-            <span class="mr-1">{{ formattedEight }}</span>
-            <span>{{ formattedNine }}</span>
-            <span>{{ formattedTen }}</span>
-            <span>{{ formattedEleven }}</span>
-            <span class="mr-1">{{ formattedTwelve }}</span>
-            <span>{{ formattedThirteen }}</span>
-            <span>{{ formattedFourteen }}</span>
-            <span>{{ formattedFifteen }}</span>
-            <span>{{ formattedSixteen }}</span>
-          </div>
-          <div v-if="getCreditCardFormat === 2">
-            <span>{{ formattedOne }}</span>
-            <span>{{ formattedTwo }}</span>
-            <span>{{ formattedThree }}</span>
-            <span class="mr-1">{{ formattedFour }}</span>
-            <span>{{ formattedFive }}</span>
-            <span>{{ formattedSix }}</span>
-            <span>{{ formattedSeven }}</span>
-            <span>{{ formattedEight }}</span>
-            <span>{{ formattedNine }}</span>
-            <span class="mr-1">{{ formattedTen }}</span>
-            <span>{{ formattedEleven }}</span>
-            <span>{{ formattedTwelve }}</span>
-            <span>{{ formattedThirteen }}</span>
-            <span>{{ formattedFourteen }}</span>
-            <span>{{ formattedFifteen }}</span>
-          </div>
+          <TweenNumber
+            v-for="(num, i) in ccIndex"
+            :start="num"
+            :end="num + 1"
+            :displayInit="ccDisplay[i]"
+            :key="i"
+            :class="{ 'mr-1': isMarginRight(num) }"
+          />
         </div>
         <v-fade-transition mode="out-in">
           <div
@@ -102,230 +75,40 @@
 </template>
 
 <script>
-import gsap from "gsap";
 import { mapGetters } from "vuex";
 
+import TweenNumber from "@/animations/TweenNumber";
+
 export default {
+  components: { TweenNumber },
   data: () => ({
     expirationDate: "MM / YY",
     cvv: "123",
     fullName: "YOUR NAME",
-
-    // Most of the code below was written to provide a tween effect on the entered credit card numbers.
-    // This effect was created using the gsap animation library (https://greensock.com/docs/v3/GSAP/gsap.to()).
-    // The reason this code is so long is because each entered digit had to be "tweened" individually for all 15
-    // or 16 digits of the credit card number.
-
-    one: { num: 1, tweenedNum: 1 },
-    two: { num: 2, tweenedNum: 2 },
-    three: { num: 3, tweenedNum: 3 },
-    four: { num: 4, tweenedNum: 4 },
-    five: { num: 1, tweenedNum: 1 },
-    six: { num: 2, tweenedNum: 2 },
-    seven: { num: 3, tweenedNum: 3 },
-    eight: { num: 4, tweenedNum: 4 },
-    nine: { num: 1, tweenedNum: 1 },
-    ten: { num: 2, tweenedNum: 2 },
-    eleven: { num: 3, tweenedNum: 3 },
-    twelve: { num: 4, tweenedNum: 4 },
-    thirteen: { num: 1, tweenedNum: 1 },
-    fourteen: { num: 2, tweenedNum: 2 },
-    fifteen: { num: 3, tweenedNum: 3 },
-    sixteen: { num: 4, tweenedNum: 4 },
+    ccIndex: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+    ccDisplay: [1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4],
   }),
 
   methods: {
-    // Tween methods defined for each individual credit card number data property object:
-    tweenOne(newNumber) {
-      gsap.to(this.one, { duration: 0.2, tweenedNum: newNumber });
-    },
-    tweenTwo(newNumber) {
-      gsap.to(this.two, { duration: 0.2, tweenedNum: newNumber });
-    },
-    tweenThree(newNumber) {
-      gsap.to(this.three, { duration: 0.2, tweenedNum: newNumber });
-    },
-    tweenFour(newNumber) {
-      gsap.to(this.four, { duration: 0.2, tweenedNum: newNumber });
-    },
-    tweenFive(newNumber) {
-      gsap.to(this.five, { duration: 0.2, tweenedNum: newNumber });
-    },
-    tweenSix(newNumber) {
-      gsap.to(this.six, { duration: 0.2, tweenedNum: newNumber });
-    },
-    tweenSeven(newNumber) {
-      gsap.to(this.seven, { duration: 0.2, tweenedNum: newNumber });
-    },
-    tweenEight(newNumber) {
-      gsap.to(this.eight, { duration: 0.2, tweenedNum: newNumber });
-    },
-    tweenNine(newNumber) {
-      gsap.to(this.nine, { duration: 0.2, tweenedNum: newNumber });
-    },
-    tweenTen(newNumber) {
-      gsap.to(this.ten, { duration: 0.2, tweenedNum: newNumber });
-    },
-    tweenEleven(newNumber) {
-      gsap.to(this.eleven, { duration: 0.2, tweenedNum: newNumber });
-    },
-    tweenTwelve(newNumber) {
-      gsap.to(this.twelve, { duration: 0.2, tweenedNum: newNumber });
-    },
-    tweenThirteen(newNumber) {
-      gsap.to(this.thirteen, { duration: 0.2, tweenedNum: newNumber });
-    },
-    tweenFourteen(newNumber) {
-      gsap.to(this.fourteen, { duration: 0.2, tweenedNum: newNumber });
-    },
-    tweenFifteen(newNumber) {
-      gsap.to(this.fifteen, { duration: 0.2, tweenedNum: newNumber });
-    },
-    tweenSixteen(newNumber) {
-      gsap.to(this.sixteen, { duration: 0.2, tweenedNum: newNumber });
+    isMarginRight(num) {
+      if (this.getCreditCardFormat === 1) {
+        if (num === 3 || num === 7 || num === 11) {
+          return true;
+        }
+      } else if (this.getCreditCardFormat === 2) {
+        if (num === 3 || num === 9) {
+          return true;
+        }
+      }
+      return false;
     },
   },
 
   computed: {
     ...mapGetters(["getFormData", "getCreditCardType", "getCreditCardFormat"]),
-    formattedOne() {
-      return Math.round(this.one.tweenedNum)
-        ? Math.round(this.one.tweenedNum)
-        : this.getFormData.cardNumber.substring(0, 1)
-        ? 0
-        : "";
-      // Double ternary operator is used for credit card numbers that are equal to 0
-      // so that 0's are also displayed, otherwise display an empty space is the value is empty.
-    },
-    formattedTwo() {
-      return Math.round(this.two.tweenedNum)
-        ? Math.round(this.two.tweenedNum)
-        : this.getFormData.cardNumber.substring(1, 2)
-        ? 0
-        : "";
-    },
-    formattedThree() {
-      return Math.round(this.three.tweenedNum)
-        ? Math.round(this.three.tweenedNum)
-        : this.getFormData.cardNumber.substring(2, 3)
-        ? 0
-        : "";
-    },
-    formattedFour() {
-      return Math.round(this.four.tweenedNum)
-        ? Math.round(this.four.tweenedNum)
-        : this.getFormData.cardNumber.substring(3, 4)
-        ? 0
-        : "";
-    },
-    formattedFive() {
-      return Math.round(this.five.tweenedNum)
-        ? Math.round(this.five.tweenedNum)
-        : this.getFormData.cardNumber.substring(4, 5)
-        ? 0
-        : "";
-    },
-    formattedSix() {
-      return Math.round(this.six.tweenedNum)
-        ? Math.round(this.six.tweenedNum)
-        : this.getFormData.cardNumber.substring(5, 6)
-        ? 0
-        : "";
-    },
-    formattedSeven() {
-      return Math.round(this.seven.tweenedNum)
-        ? Math.round(this.seven.tweenedNum)
-        : this.getFormData.cardNumber.substring(6, 7)
-        ? 0
-        : "";
-    },
-    formattedEight() {
-      return Math.round(this.eight.tweenedNum)
-        ? Math.round(this.eight.tweenedNum)
-        : this.getFormData.cardNumber.substring(7, 8)
-        ? 0
-        : "";
-    },
-    formattedNine() {
-      return Math.round(this.nine.tweenedNum)
-        ? Math.round(this.nine.tweenedNum)
-        : this.getFormData.cardNumber.substring(8, 9)
-        ? 0
-        : "";
-    },
-    formattedTen() {
-      return Math.round(this.ten.tweenedNum)
-        ? Math.round(this.ten.tweenedNum)
-        : this.getFormData.cardNumber.substring(9, 10)
-        ? 0
-        : "";
-    },
-    formattedEleven() {
-      return Math.round(this.eleven.tweenedNum)
-        ? Math.round(this.eleven.tweenedNum)
-        : this.getFormData.cardNumber.substring(10, 11)
-        ? 0
-        : "";
-    },
-    formattedTwelve() {
-      return Math.round(this.twelve.tweenedNum)
-        ? Math.round(this.twelve.tweenedNum)
-        : this.getFormData.cardNumber.substring(11, 12)
-        ? 0
-        : "";
-    },
-    formattedThirteen() {
-      return Math.round(this.thirteen.tweenedNum)
-        ? Math.round(this.thirteen.tweenedNum)
-        : this.getFormData.cardNumber.substring(12, 13)
-        ? 0
-        : "";
-    },
-    formattedFourteen() {
-      return Math.round(this.fourteen.tweenedNum)
-        ? Math.round(this.fourteen.tweenedNum)
-        : this.getFormData.cardNumber.substring(13, 14)
-        ? 0
-        : "";
-    },
-    formattedFifteen() {
-      return Math.round(this.fifteen.tweenedNum)
-        ? Math.round(this.fifteen.tweenedNum)
-        : this.getFormData.cardNumber.substring(14, 15)
-        ? 0
-        : "";
-    },
-    formattedSixteen() {
-      return Math.round(this.sixteen.tweenedNum)
-        ? Math.round(this.sixteen.tweenedNum)
-        : this.getFormData.cardNumber.substring(15, 16)
-        ? 0
-        : "";
-    },
   },
 
   watch: {
-    // Grab each number separately from the Vuex state of formData to achieve an individual
-    // tween animation for each credit card number:
-    "$store.getters.getFormData"() {
-      this.one.num = Number(this.getFormData.cardNumber.substring(0, 1));
-      this.two.num = Number(this.getFormData.cardNumber.substring(1, 2));
-      this.three.num = Number(this.getFormData.cardNumber.substring(2, 3));
-      this.four.num = Number(this.getFormData.cardNumber.substring(3, 4));
-      this.five.num = Number(this.getFormData.cardNumber.substring(4, 5));
-      this.six.num = Number(this.getFormData.cardNumber.substring(5, 6));
-      this.seven.num = Number(this.getFormData.cardNumber.substring(6, 7));
-      this.eight.num = Number(this.getFormData.cardNumber.substring(7, 8));
-      this.nine.num = Number(this.getFormData.cardNumber.substring(8, 9));
-      this.ten.num = Number(this.getFormData.cardNumber.substring(9, 10));
-      this.eleven.num = Number(this.getFormData.cardNumber.substring(10, 11));
-      this.twelve.num = Number(this.getFormData.cardNumber.substring(11, 12));
-      this.thirteen.num = Number(this.getFormData.cardNumber.substring(12, 13));
-      this.fourteen.num = Number(this.getFormData.cardNumber.substring(13, 14));
-      this.fifteen.num = Number(this.getFormData.cardNumber.substring(14, 15));
-      this.sixteen.num = Number(this.getFormData.cardNumber.substring(15, 16));
-    },
-
     // Listen to changes in the Vuex state for the formattedFormData property and update the values on the
     // card images accordingly:
     "$store.getters.getFormattedFormData"(newValue) {
@@ -335,104 +118,6 @@ export default {
       this.cvv = newValue.cvv.length ? newValue.cvv : "123";
       const fullName = newValue.firstName + " " + newValue.lastName;
       this.fullName = fullName.length > 1 ? fullName : "YOUR NAME";
-    },
-
-    // Listed to changes on each credit card number and run the tween method on each value change:
-    one: {
-      handler(newValue) {
-        this.tweenOne(newValue.num);
-      },
-      deep: true,
-    },
-    two: {
-      handler(newValue) {
-        this.tweenTwo(newValue.num);
-      },
-      deep: true,
-    },
-    three: {
-      handler(newValue) {
-        this.tweenThree(newValue.num);
-      },
-      deep: true,
-    },
-    four: {
-      handler(newValue) {
-        this.tweenFour(newValue.num);
-      },
-      deep: true,
-    },
-    five: {
-      handler(newValue) {
-        this.tweenFive(newValue.num);
-      },
-      deep: true,
-    },
-    six: {
-      handler(newValue) {
-        this.tweenSix(newValue.num);
-      },
-      deep: true,
-    },
-    seven: {
-      handler(newValue) {
-        this.tweenSeven(newValue.num);
-      },
-      deep: true,
-    },
-    eight: {
-      handler(newValue) {
-        this.tweenEight(newValue.num);
-      },
-      deep: true,
-    },
-    nine: {
-      handler(newValue) {
-        this.tweenNine(newValue.num);
-      },
-      deep: true,
-    },
-    ten: {
-      handler(newValue) {
-        this.tweenTen(newValue.num);
-      },
-      deep: true,
-    },
-    eleven: {
-      handler(newValue) {
-        this.tweenEleven(newValue.num);
-      },
-      deep: true,
-    },
-    twelve: {
-      handler(newValue) {
-        this.tweenTwelve(newValue.num);
-      },
-      deep: true,
-    },
-    thirteen: {
-      handler(newValue) {
-        this.tweenThirteen(newValue.num);
-      },
-      deep: true,
-    },
-    fourteen: {
-      handler(newValue) {
-        this.tweenFourteen(newValue.num);
-      },
-      deep: true,
-    },
-    fifteen: {
-      handler(newValue) {
-        this.tweenFifteen(newValue.num);
-      },
-      deep: true,
-    },
-    sixteen: {
-      handler(newValue) {
-        this.tweenSixteen(newValue.num);
-      },
-      deep: true,
     },
   },
 };
